e314d80d921649d67aaa59a9e7c4de70c5c1eed7
Adele McKenna <theotheradele@gmail.com>
2024-04-05 16:36:46 -0400

    Added search form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
-     Hello
       </header>
+      <main>
+        <Dictionary/>
+      </main>
+      <footer className="app-footer"><small>This project was coded by Adele McKenna and is open-sourced on Github</small></footer>
+      </div>
     </div>
   );
 }
