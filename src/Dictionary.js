0feb32bf3d81b3f9491923314b90f96db438bc3b
Adele McKenna <theotheradele@gmail.com>
2024-04-18 16:29:41 -0400

    API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
  let [keyword, setKeyword] = useState("");
-  function search(event) {
+ 
+ function handleResponse(response) {
+console.log(response.data)
+ } 
+ 
+function search(event) {
 event.preventDefault();
-alert(`Searching for ${keyword}`);
- }
+let key="0ocfta5c0e4602a2a90c32a9a4bbf5b9"
+let word= keyword;
+let apiUrl=`https://api.shecodes.io/dictionary/v1/define?word=${word}&key=${key}`;
+axios.get(apiUrl).then(handleResponse);
+}
 
  function handleKeywordChange(event) {
   setKeyword(event.target.value);
