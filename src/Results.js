d4887411d35dbca6a05c9a4715bf6160a306220f
Adele McKenna <theotheradele@gmail.com>
2024-04-26 17:14:28 -0400

    Added Meaning Component

--- src/Results.js
+++ src/Results.js
@@ -1,14 +1,19 @@
 import React from "react";
+import Meaning from "./Meaning";
 
 export default function Results(props) {
-  console.log(props.results);
   if (props.results) {
   return (
     <div className = "results">
   <h2>{props.results.word}</h2>
 {props.results.meanings.map(function(meaning, index) {
-  return meanings[0].definition;
+  return (<div key={index}>
+<Meaning meaning={meaning}/>
+    </div>
+  )
 })}
   </div> );}
-  else {return null;}
+  else {
+    return null;
+  }
 }
