db02d4b22b0d0c08d64246b74daf31fccb7273ce
Adele McKenna <theotheradele@gmail.com>
2024-04-26 18:04:58 -0400

    Added Synonyms Component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,12 +1,22 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
 console.log(props.meaning);
   return ( 
   <div className="Meaning">
   <h3>{props.meaning.partOfSpeech}</h3>
-<p>{props.meaning.definition}</p>
-<p>{props.meaning.example}</p>
+<p>
+  <strong>Definition:</strong>
+  {props.meaning.definition}
+<br/>
+  <strong>Example:</strong>
+  <em>
+{props.meaning.example}
+</em>
+<br/>
+<Synonyms synonyms={props.meaning.synonyms}/>
+</p>
   </div>
  );
   }
---
+++ src/Synonyms.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Synonyms(props) {
+  if (props.synonyms) {
+ return "Hello from Synonyms"
+  }
+  else {
+    return null;
+  }
+}
