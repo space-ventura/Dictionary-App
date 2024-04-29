125052b982324b9cbec0cbc6319ef1d7921ca602
Adele McKenna <theotheradele@gmail.com>
2024-04-28 20:13:15 -0400

    Added image section

--- src/App.js
+++ src/App.js
@@ -7,9 +7,10 @@ function App() {
     <div className="App">
       <div className="container">
       <header className="App-header">
+        <h1>Dictionary App</h1>
       </header>
       <main>
-        <Dictionary defaultKeyword="Genocide"/>
+        <Dictionary defaultKeyword="Opossum"/>
       </main>
       <footer className="app-footer"><small>This project was coded by Adele McKenna and is open-sourced on Github</small></footer>
       </div>
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,23 +2,33 @@ import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
 import Results from "./Results.js";
+import Photos from "./Photos.js";
 
 export default function Dictionary(props) {
  let [keyword, setKeyword] = useState(props.defaultKeyword);
  let [results, setResults] = useState(null);
 let [loaded, setLoaded] = useState(false);
+let [photos, setPhotos] = useState(null);
  function handleResponse(response) {
-console.log(response.data.meanings[0].definition)
 setResults(response.data)
+
 } 
 
+function handleImageResponse(response) {
+setPhotos(response.data.photos);
+}
+
 function search () {
-let key="0ocfta5c0e4602a2a90c32a9a4bbf5b9"
+let key="0ocfta5c0e4602a2a90c32a9a4bbf5b9";
 let word= keyword;
 let apiUrl=`https://api.shecodes.io/dictionary/v1/define?word=${word}&key=${key}`;
 axios.get(apiUrl).then(handleResponse);
+let imageApiKey= "0ocfta5c0e4602a2a90c32a9a4bbf5b9";
+let imageApiUrl= `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${imageApiKey}`;
+axios.get(imageApiUrl).then(handleImageResponse);
 }
  
+
 function handleSubmit(event) {
 event.preventDefault();
 search();
@@ -38,15 +48,16 @@ if (loaded) {
 return (
     <div className="Dictionary">
 <section>
-  <h1>What word do you seek meaning for?</h1>
+  <h2>What word do you seek meaning for?</h2>
 <form onSubmit={handleSubmit}>
   <input type="search" defaultValue={props.defaultKeyword} onChange={handleKeywordChange}/>
 </form>
 <div className="hint">
-  example: ceasefire, now, free, Palestine, etc.
+  example: river, sea, ceasefire, now, etc.
 </div>
 </section>
 <Results results={results}/>
+<Photos photos={photos}/>
 </div>
   ); 
 }
